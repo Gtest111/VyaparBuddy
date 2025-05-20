aca871a56e552184668a65760d45e654dd0be017
Yajuvendrasinh Chudasama <146720362+Gtest111@users.noreply.github.com>
2025-05-20 17:40:01 +0530

    Delete backend directory

--- backend/models/Post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const postSchema = new mongoose.Schema({
-    postContent: { type: String, required: true },
-    scheduleTime: { type: Date, required: true }
-});
-
-module.exports = mongoose.model("Post", postSchema);
